f6524eb4f577877a5b94c4dd0e3d1f76a3f4767a
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-08-19 01:42:57 +0200

    Add the new entity Users and your repository

---
+++ src/Entity/Users.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Created at 8/19/20, 1:19 AM
+ * For the project APIREST
+ * For Alexandre CONTE
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Users
+ * @package App\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ */
+class Users
+{
+
+}
