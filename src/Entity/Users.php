37306ba332691928582cc62c9f3cffbdbcc6b723
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-08-19 09:13:55 +0200

    Update Entity Users with attributs

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -13,8 +13,117 @@ use Doctrine\ORM\Mapping as ORM;
  * Class Users
  * @package App\Entity
  * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ *
+ * @author CONTE Alexandre <pro.alexandre.conte@gmail.com>
  */
 class Users
 {
+    /**
+     * @var int
+     *
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     */
+    private int $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=100, nullable=true)
+     */
+    private string $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=125, nullable=true)
+     */
+    private string $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=80, nullable=false)
+     */
+    private string $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=250, nullable=false)
+     */
+    private string $password;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
 }
